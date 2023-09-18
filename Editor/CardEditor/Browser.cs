e6d241ba25569b430e5dcbfd3cddbae438b657bc
Long18 <us.thanhlong18@gmail.com>
2023-09-18 23:03:59 +0700

    refactor(browser): using ui toolkit

---
+++ Editor/CardEditor/Browser.cs
@@ -0,0 +1,184 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Long18.SOBrowser;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Object = UnityEngine.Object;
+
+namespace Long18.Tools
+{
+    public class Browser : EditorWindow
+    {
+        [SerializeField] private VisualTreeAsset _uxml;
+        [SerializeField] private StyleSheet _styleSheet;
+
+        private static readonly List<Type> _browsableTypes = new();
+        private static string[] _browsableTypesName = Array.Empty<string>();
+        private static Dictionary<Type, BrowserEditor> _editors;
+
+        protected List<AssetEntry> _assetList = new();
+        protected List<AssetEntry> _sortedAssetList = new();
+
+        private BrowserEditor _currentEditor;
+        private Object _currentObject;
+
+        protected int _currentTypeIndex;
+
+        private ListView _listView;
+        private Box _cardInfoBox;
+
+        [MenuItem("Long 18/Tool Cards")]
+        public static Browser ShowWindow()
+        {
+            ReloadBrowserEditors();
+
+            Browser[] windows = Resources.FindObjectsOfTypeAll<Browser>();
+            if (windows.Length > 0) return windows[0];
+
+            Browser window = GetWindow<Browser>();
+
+            window.ShowTab();
+            return window;
+        }
+
+        private void OnEnable()
+        {
+            InitUI();
+
+            ReloadBrowserEditors();
+            CreateListView();
+        }
+
+        #region UI
+
+        private void InitUI()
+        {
+            rootVisualElement.Add(_uxml.CloneTree());
+            rootVisualElement.styleSheets.Add(_styleSheet);
+
+            _listView = rootVisualElement.Q<ListView>("card-list");
+            _cardInfoBox = rootVisualElement.Q<Box>("card-info");
+        }
+
+        private void CreateListView()
+        {
+            Type currentType = null;
+
+            if (_currentEditor == null && _currentObject != null)
+            {
+                OpenObject(_currentObject);
+            }
+            else if (_browsableTypes.Count > 0)
+            {
+                currentType = GetType(_browsableTypes[0]);
+            }
+
+            if (currentType == null) return;
+
+            FindDatas(out Object[] cards, currentType.Name);
+
+            _listView.itemsSource = cards;
+            _listView.fixedItemHeight = 16;
+            _listView.selectionType = SelectionType.Single;
+
+            _listView.onSelectionChange += OnChoosingItem;
+            _listView.RefreshItems();
+        }
+
+
+        private void OnChoosingItem(IEnumerable<object> items)
+        {
+            foreach (var item in items)
+            {
+                _cardInfoBox.Clear();
+                var data = item;
+
+                SerializedObject sObject = new SerializedObject((Object)data);
+                SerializedProperty sDataProperty = sObject.GetIterator();
+                sDataProperty.Next(true);
+
+                while (sDataProperty.NextVisible(false))
+                {
+                    PropertyField prop = new PropertyField(sDataProperty);
+                    prop.Bind(sObject);
+                    _cardInfoBox.Add(prop);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Over Engineer
+
+        private static void ReloadBrowserEditors()
+        {
+            if (_editors != null) return;
+
+            _editors = new();
+            List<string> typesName = new List<string>();
+
+            Assembly[] allFiles = AppDomain.CurrentDomain.GetAssemblies();
+
+            foreach (var file in allFiles)
+            {
+                foreach (var type in file.GetTypes())
+                {
+                    if (type.BaseType is not { IsGenericType: true }) continue;
+                    if (type.BaseType.GetGenericTypeDefinition() != typeof(BrowserEditor<>)) continue;
+                    if (type.BaseType == null && _editors != null) return;
+
+                    Type genericArgument = type.BaseType?.GetGenericArguments()[0];
+                    _editors[genericArgument!] = (BrowserEditor)Activator.CreateInstance(type);
+
+                    _browsableTypes.Add(genericArgument);
+                    typesName.Add(genericArgument?.Namespace);
+                }
+            }
+
+            _browsableTypesName = _browsableTypesName.ToArray();
+        }
+
+        private static void OpenObject(Object obj)
+        {
+            Browser window = ShowWindow();
+            Type type = obj.GetType();
+
+            window.GetType(type);
+            BrowserEditor editor = window._currentEditor;
+
+            Object[] target = { obj };
+            editor.SetTargetObjects(target);
+        }
+
+        private Type GetType(Type type)
+        {
+            while (type != null && _editors.ContainsKey(type) == false) type = type.BaseType;
+            if (type == null) return null;
+
+            _currentTypeIndex = _browsableTypes.IndexOf(type);
+            _currentEditor = _editors[type];
+            _currentEditor.Editor_SetBrowser(this);
+
+            return type;
+        }
+
+        private void FindDatas<T>(out T[] data, string name = "") where T : Object
+        {
+            var guids = AssetDatabase.FindAssets($"t:{name}");
+
+            data = new T[guids.Length];
+
+            for (var index = 0; index < guids.Length; index++)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guids[index]);
+                data[index] = AssetDatabase.LoadAssetAtPath<T>(path);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Editor/CardEditor/BrowserEditor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Long18.Tools
+{
+    public abstract class BrowserEditor
+    {
+        protected Editor CachedEditor;
+        public Browser Browser { get; protected set; }
+        public bool CreateDataFolder { get; protected set; }
+        public string DefaultStoragePath { get; protected set; }
+        public GenericMenu ContextMenu { get; protected set; }
+
+        public virtual void SetTargetObjects(Object[] objects)
+        {
+        }
+
+        public virtual void RenderInspector()
+        {
+        }
+
+        /// <summary>
+        /// This method is called when user clicks on "Import" button in SOBrowser window
+        ///  - directory is the directory of the selected file 
+        /// </summary>
+        /// <param name="directory">Directory of input file</param>
+        /// <param name="callback">callback for adding new ScriptableObject to ListView</param>
+        public virtual void ImportBatchData(string directory, Action<ScriptableObject> callback)
+        {
+        }
+
+#if UNITY_EDITOR
+        /// <summary>
+        /// This function only used for Editor
+        /// </summary>
+        /// <param name="value"></param>
+        public void Editor_SetBrowser(Browser value) => Browser = value;
+#endif
+    }
+
+    public abstract class BrowserEditor<T> : BrowserEditor where T : Object
+    {
+        public T TargetObject { get; set; }
+
+        public override void SetTargetObjects(Object[] objects)
+        {
+            if (objects == null || objects.Length <= 0) TargetObject = null;
+            else TargetObject = (T)objects[0];
+
+            Editor.CreateCachedEditor(objects, null, ref CachedEditor);
+            if (CachedEditor != null) CachedEditor.ResetTarget();
+        }
+
+        public override void RenderInspector()
+        {
+            if (TargetObject == null) return;
+            CustomInspector(CachedEditor.serializedObject);
+        }
+
+        private void DrawDefaultInspector() => CachedEditor.OnInspectorGUI();
+
+        protected virtual void CustomInspector(SerializedObject objects) => DrawDefaultInspector();
+
+        public override void ImportBatchData(string directory, Action<ScriptableObject> callback)
+        {
+        }
+    }
+}
